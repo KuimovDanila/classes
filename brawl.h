db1b9c869a643d06892ba41a82e96a84b8931e3d
Danil_Plavnik <59319963+danilplavnik@users.noreply.github.com>
2024-03-11 17:09:40 +0300

    Поправил ошибочки

--- brawl.h
+++ brawl.h
@@ -1,3 +1,4 @@
+#include <string>
 class Brawler {
 protected:
     int hp;
@@ -25,76 +26,76 @@ public:
     string GetRarity();
     int GetRang();
     int GetLevel();
-}
+};
 class Tank: public Brawler {
 
 public:
     Tank();
     string GetRange();
-}
+};
 
 class Support: public Brawler {
 private:
     bool CanHill;
 public:
     Support();
-}
+};
 
 class Thrower: public Brawler {
 public:
     Thrower();
     string GetRange();
-}
+};
 
 class Sniper: public Brawler {
 
 
 public:
-    Sniper()
+    Sniper();
     string GetRange();
-}
+};
 
 class Assasin: public Brawler {
-
-public:Assasin()
-}
+public:
+    Assasin();
+};
 
 class Piper: public Sniper {
 public:
 
     Piper(int level, int rang);
     void say();
-}
+};
 class ElPrimo: public Tank {
 public:
     ElPrimo(int level, int rang);
     void say();
-}
+};
 class Bull: public Tank {
 public:
     Bull(int level, int rang);
     void say();
-}
+};
 class Leon: public Assasin {
 public:
     Leon(int level, int rang);
     void say();
-}
+};
 class Spike: public Assasin {
 public:
     Spike(int level, int rang);
     void say();
-}
+};
 class Dynamike: public Thrower {
 public:
     Dynamike(int level, int rang);
     void say();
-}
+};
 class Poco: public Support {
 public:
     Poco(int level, int rang);
     void say();
-}
+};
 
 
 
